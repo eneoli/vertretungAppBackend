1e7c23048a1910cddf466575037e6a8dc9160f2d
Oliver Enes <oliver@enesnet.de>
2020-08-16 02:55:25 +0200

    /secureMoodleSession

--- src/controller/MoodleSessionController.ts
+++ src/controller/MoodleSessionController.ts
@@ -5,6 +5,8 @@ import {Request, Response} from 'express';
 import {MoodleSession} from '../service/MoodleSession';
 import {Settings} from '../service/Settings';
 import {ParamsDictionary} from 'express-serve-static-core';
+import {Crypt} from '../service/Crypt';
+import {KeyManager} from '../service/KeyManager';
 
 interface CreatQuery extends ParamsDictionary {
     username: string;
@@ -13,6 +15,10 @@ interface CreatQuery extends ParamsDictionary {
 
 interface ValidateQuery {
     moodleSession: string;
+}
+
+interface SecureQuery {
+    secret: string;
 }
 
 export class MoodleSessionController extends Controller {
@@ -52,6 +58,36 @@ export class MoodleSessionController extends Controller {
             res.json({
                 error: e.message,
             });
+        }
+    }
+
+    @Route('/secureMoodleSession', HTTPMethods.get)
+    public async onSecureMoodleSession(req: Request<{}, {}, {}, SecureQuery>, res: Response) {
+        const settings = new Settings();
+        settings.load('./config.yml');
+        const session = new MoodleSession(settings);
+        const keyManager = new KeyManager(settings);
+
+        if (req.query.secret !== undefined) {
+            let decrypted = '';
+            try {
+                decrypted = Crypt.decrypt(keyManager.getPrivateKy(), req.query.secret);
+            } catch (e) {
+                res.send({error: 'Serverfehler'});
+            }
+
+            const decryptedObject = JSON.parse(decrypted);
+            try {
+                const moodleSession =
+                    await session.obtainMoodleSession(decryptedObject.username, decryptedObject.password);
+                res.json({moodleSession: moodleSession});
+            } catch (e) {
+                res.json({
+                    error: e.message
+                });
+            }
+        } else {
+            res.send({error: 'Bitte gib Benutzername und Passwort an'});
         }
     }
 }
